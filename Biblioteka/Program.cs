95e679dd0612d3e105970b6c4a10faa3d71a7530
dell <dell@DESKTOP-JU0NP8G>
2023-11-19 10:35:57 +0100

    Add project files.

---
+++ Biblioteka/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Good morning, this is your Library");
+
+        string napis = "Mój pierwszy string";
+        Console.WriteLine(napis);
+        int liczba = 5;
+        Console.WriteLine(liczba);
+
+        string opcja = Console.ReadLine();
+
+        if(opcja == 1)
+}
+}
